5ec0ba9891cc37162a0ebe2aee0d66ba5a3a37c8
Rodrigo Hernandez <rjhernandr@gmail.com>
2020-06-15 19:50:14 -0400

    Added env_vars and requirements files

---
+++ env_vars.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set DB_USER=wys
+set DB_PASSWORD=rac3e/07
+set DB_IP_ADDRESS=10.2.19.195
+set DB_PORT=3307
+set DB_SCHEMA=wys
+set FLASK_APP=main.py
+
+
+
+echo %DB_USER%
+echo %DB_PASSWORD%
+echo %DB_IP_ADDRESS%
+echo %DB_PORT%
+echo %DB_SCHEMA%
+@echo on
